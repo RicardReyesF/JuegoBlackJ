83161927657276c80580115907d5d6a0b4a973a2
unknown <rreyesfdz96@gmail.com>
2022-09-30 00:26:35 -0500

    Carta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,6 +1,11 @@
 let deck = [];
 let tipos =['C','D','H','S'];
 let especiales = ['A','J','Q','K'];
+let contJugador = 0,
+    contMaquina = 0;
+const pedirC            = document.querySelector('#btnCartaN');
+const marcador          = document.querySelector('small');
+const cartasHTMLJugador = document.querySelector('#jugador-cartas');
 
 
 const crearDeck = () => {
@@ -12,11 +17,11 @@ const crearDeck = () => {
     }
     for (tipo of tipos) {
         for (esp of especiales) {
-            deck.push(tipo + esp);
+            deck.push(esp + tipo);
         }
     }
     deck = _.shuffle(deck);
-    console.log(deck);
+    
 }
 
 crearDeck();
@@ -26,16 +31,35 @@ const pedirCarta = () => {
         throw "La baraja esta vacia";
     }
     const carta = deck.pop();
-    console.log(carta);
     return carta;
 }
 
-pedirCarta();
+//pedirCarta();
 
 
 const valorCarta = (carta) => {
     const valor = carta.substring(0,carta.length -1);
-    let punto = 0;
+    return (isNaN(valor)) ? 
+            (valor === 'A')   ? 11 : 10
+            : valor * 1;
 }
 
-valorCarta("2D");
+pedirC.addEventListener('click',function () {
+    const carta = pedirCarta();
+
+    contJugador = contJugador + valorCarta( carta );
+    marcador.innerText = contJugador;
+    
+    const imgCarta = document.createElement('img');
+    imgCarta.src = `assets/cartas/${ carta }.png`;
+    imgCarta.classList.add('carta');
+    cartasHTMLJugador.append(imgCarta);
+
+    if(contJugador > 21){
+        pedirC.disabled = true;
+        console.warn("Valio Berga Chuek!")
+    }else if ( contJugador === 21){
+        console.warn('Ah perro, 21!!!');
+    }
+});
+
